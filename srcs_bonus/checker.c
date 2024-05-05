95b84c0fe400f36f818a006ce24adbdf1a0d09fe
Hicham Es-Saquy <hes-saqu@c4r9p4.1337.ma>
2024-05-05 13:32:38 +0100

    error managing

--- srcs_bonus/checker.c
+++ srcs_bonus/checker.c
@@ -109,6 +109,8 @@ int	main(int ac, char **av)
 	head = ft_parse(ac, av);
 	len = stack_len(head);
 	values = stack_to_array(head, len);
+	if (!values)
+		ft_error(values, head, 0);
 	if (check_duplicates(values, len) < 0)
 		ft_error(values, head, 2);
 	free(values);
